7699d728f3692c1a950f3c56ce8d29dcac336ba4
unknown <urbantour0809@gmail.com>
2024-10-18 21:22:30 +0900

    unified result and index html

--- script.js
+++ script.js
@@ -13,26 +13,34 @@ window.onload = function() {
 
     console.log("Fetching lotto numbers from backend...");
 
-    fetch(`${flaskUrl}/generate-lotto`)
-        .then(response => {
-            console.log("Received response from backend");
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log("Lotto numbers:", data.numbers);
-            const gameNumbers = data.numbers;
-            appendNumbers('game1', gameNumbers[0]);
-            appendNumbers('game2', gameNumbers[1]);
-            appendNumbers('game3', gameNumbers[2]);
-            appendNumbers('game4', gameNumbers[3]);
-            appendNumbers('game5', gameNumbers[4]);
-        })
-        .catch(error => {
-            console.error('Error fetching lotto numbers:', error);
-        });
+    function generateLotto() {
+        document.getElementById('loading').style.display = 'flex'; // 로딩 중 표시
+
+        fetch(`${flaskUrl}/generate-lotto`)
+            .then(response => {
+                console.log("Received response from backend");
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log("Lotto numbers:", data.numbers);
+                const gameNumbers = data.numbers;
+                appendNumbers('game1', gameNumbers[0]);
+                appendNumbers('game2', gameNumbers[1]);
+                appendNumbers('game3', gameNumbers[2]);
+                appendNumbers('game4', gameNumbers[3]);
+                appendNumbers('game5', gameNumbers[4]);
+
+                document.getElementById('loading').style.display = 'none'; // 로딩 숨김
+                document.getElementById('result-container').style.display = 'block'; // 결과 표시
+            })
+            .catch(error => {
+                console.error('Error fetching lotto numbers:', error);
+                document.getElementById('loading').style.display = 'none'; // 로딩 숨김
+            });
+    }
 
     function appendNumbers(gameId, numbers) {
         console.log(`Appending numbers for ${gameId}:`, numbers);
@@ -40,10 +48,12 @@ window.onload = function() {
         gameDiv.innerHTML = '';
         numbers.forEach(num => {
             const img = document.createElement('img');
-            img.src = `src/image/numbers/${num}.png`;
+            img.src = `src/image/numbers/${num}.png`; // 번호에 해당하는 이미지를 로드
             img.alt = `${num}번`;
             img.classList.add('lotto-ball');
             gameDiv.appendChild(img);
         });
     }
-}
+
+    window.generateLotto = generateLotto;  // 전역에서 사용 가능하도록
+};
